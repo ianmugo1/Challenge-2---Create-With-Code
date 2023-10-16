928b2f6045d35bc7353ec40041ba276a00cadce8
ianmugo1 <113984797+ianmugo1@users.noreply.github.com>
2023-10-16 10:29:03 +0100

    Create with code

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    
+    public GameObject dogPrefab;
+    public float timer;
+    public float timeLimit = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Create timer
+        timer += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& timer > timeLimit)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // reset timer
+            timer = 0; 
+        }
+    }
+}
